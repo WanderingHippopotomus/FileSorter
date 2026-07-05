30306664c92c42cf21f5b61c762418eb8de891de
WanderingHippopotomus <epicthings26@gmail.com>
2026-07-05 20:06:21 +0530

    Refactor document sorting and improve file organization

--- backend/classifier.py
+++ backend/classifier.py
@@ -4,7 +4,12 @@ import re
 
 img_re = '.*\.(jpg|jpeg|png|gif)'
 vid_re = '.*\.(mp4|mp3|mov|m4a)'
-docs_re = '.*\.(pdf|txt|docx|xlsx|xls|csv|ppt|pptx)'
+docs_re = '.*\.(pdf|txt|docx|xlsx|xls|csv|ppt|pptx|doc)'
+word_re = '.*\.(docx|doc)'
+txt_re = '.*\.txt'
+pdf_re = '.*\.(pdf)'
+excel_re = '.*\.(xlsx|xls|csv)'
+ppt_re = '.*\.(ppt|pptx)'
 misc_re = '.*\..*'
 
 def classify_files() -> list | None:
@@ -13,6 +18,11 @@ def classify_files() -> list | None:
     imgs = []
     vids = []
     docs = []
+    word = []
+    excel = []
+    pdf = []
+    ppt = []
+    txt = []
     misc = []
     
     images = None
@@ -31,6 +41,18 @@ def classify_files() -> list | None:
             if re.match(misc_re, files[i]):
                 misc.append(files[i])
     
+    for i in range(len(docs)):
+        if re.match(word_re, docs[i]):
+            word.append(docs[i])
+        elif re.match(excel_re, docs[i]):
+            excel.append(docs[i])
+        elif re.match(pdf_re, docs[i]):
+            pdf.append(docs[i])
+        elif re.match(ppt_re, docs[i]):
+            ppt.append(docs[i])
+        elif re.match(txt_re, docs[i]):
+            txt.append(docs[i])
+            
     if 'img' in SORT:
         images = imgs
     
@@ -39,8 +61,13 @@ def classify_files() -> list | None:
     
     if 'docs' in SORT:
         documents = docs
-    
+        word_doc = word
+        txt_doc = txt
+        ppt_doc = ppt
+        excel_doc = excel
+        pdf_doc = pdf
+        
     if 'misc' in SORT:
         miscs = misc
         
-    return [images, videos, documents, miscs]
+    return [images, videos, documents, word_doc, txt_doc, ppt_doc, excel_doc, pdf_doc, miscs]
--- backend/sorter.py
+++ backend/sorter.py
@@ -13,21 +13,40 @@ def create_dir():
         os.mkdir('Videos')
     except FileExistsError:
         logger.warning("Folder Videos already exist.")
-    try:
-        os.mkdir('Documents')
-    except FileExistsError:
-        logger.warning("Folder Documents already exist.")
+
+    os.makedirs('Documents\Word', exist_ok=True)
+    os.makedirs('Documents\PPT', exist_ok=True)
+    os.makedirs('Documents\PDF', exist_ok=True)
+    os.makedirs('Documents\Excel', exist_ok=True)
+    os.makedirs('Documents\Txt', exist_ok=True)
+        
     try:
         os.mkdir('Miscs')
     except FileExistsError:
         logger.warning("Folder Miscs already exist.")
 
-def sort_main(DIR):
+def _move_files(DIR,files,folder):
+    moved = 0
+    for item in files:
+            try:
+                shutil.move(f'{DIR}\{item}', f'{DIR}\Documents\{folder}')
+                moved += 1
+            except (PermissionError, FileNotFoundError):
+                logger.error(f"{item} not found or moved, or not enough permission.")
+    
+    return moved
+                
+def sort_main(DIR:str) -> tuple:
     classified_files = classify_files()
     imgs = classified_files[0]
     vids = classified_files[1]
     docs = classified_files[2]
-    miscs = classified_files[3]
+    word_doc = classified_files[3]
+    txt_doc = classified_files[4]
+    ppt_doc = classified_files[5]
+    excel_doc = classified_files[6]
+    pdf_doc = classified_files[7]
+    miscs = classified_files[8]
     
     imgs_counter = 0
     if imgs:
@@ -50,16 +69,17 @@ def sort_main(DIR):
                 logger.error(f"{vid} not found or moved, or not enough permission.")
                 
     logger.info(f'Moved {vids_counter} videos.')
-        
-    docs_counter = 0
+    
+    counter1 = counter2 = counter3 = counter4 = counter5 = 0 
     if docs:
-        for doc in docs:
-            try:
-                shutil.move(f'{DIR}\{doc}', f'{DIR}\Documents')
-                docs_counter += 1
-            except (PermissionError, FileNotFoundError):
-                logger.error(f"{doc} not found or moved, or not enough permission.")
-                
+        counter1 = _move_files(DIR,word_doc, 'Word')
+        counter2 = _move_files(DIR,pdf_doc, 'PDF')
+        counter3 = _move_files(DIR,excel_doc, 'Excel')
+        counter4 = _move_files(DIR,ppt_doc, 'PPT')
+        counter5 = _move_files(DIR,txt_doc, 'Txt')
+    
+    docs_counter = counter1 + counter2 + counter3 + counter4 + counter5
+    
     logger.info(f'Moved {docs_counter} documents.')
         
     misc_counter = 0
@@ -76,4 +96,5 @@ def sort_main(DIR):
     total_files = len(imgs or []) + len(miscs or []) + len(vids or []) + len(docs or [])
     successfully_moved = imgs_counter + vids_counter + docs_counter + misc_counter
     error = total_files - successfully_moved
+    
     return total_files, successfully_moved, error
